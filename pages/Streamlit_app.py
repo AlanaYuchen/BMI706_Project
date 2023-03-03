6e4d5631a1b52737be5fa49e0d6e237a7580ec50
Jesslyn <jgoh@hms.harvard.edu>
2023-03-03 10:54:14 -0500

    create streamlit app

--- pages/Streamlit_app.py
+++ pages/Streamlit_app.py
@@ -9,39 +9,46 @@ Created on Fri Mar  3 09:47:14 2023
 import pandas as pd
 import numpy as np
 import altair as alt
+import streamlit as st
 
-clinical_df = pd.read_csv("clinical.tsv", delimiter = "\t")
-family_df = pd.read_csv("family_history.tsv", delimiter = "\t")
+@st.cache_data
+def load_data():
+  clinical_df = pd.read_csv("clinical.tsv", delimiter = "\t")
+  family_df = pd.read_csv("family_history.tsv", delimiter = "\t")
 
-# ================ clean clinical_df data ===============
-# replace '-- with empty space and collapse columns with cancer stage to one
-clinical_df = clinical_df.replace("'--","")
-clinical_df = clinical_df.fillna('')
+  # ================ clean clinical_df data ===============
+  # replace '-- with empty space and collapse columns with cancer stage to one
+  clinical_df = clinical_df.replace("'--","")
+  clinical_df = clinical_df.fillna('')
 
-stage_columns = clinical_df.filter(regex=("stage.*"))
-stage_columns.drop(axis = 1, columns = ["ajcc_clinical_stage", "ann_arbor_clinical_stage"])
-agg_df = stage_columns.agg(lambda x: ''.join(x.astype(str)), axis=1)
-clinical_df['stage'] = agg_df
+  stage_columns = clinical_df.filter(regex=("stage.*"))
+  stage_columns.drop(axis = 1, columns = ["ajcc_clinical_stage", "ann_arbor_clinical_stage"])
+  agg_df = stage_columns.agg(lambda x: ''.join(x.astype(str)), axis=1)
+  clinical_df['stage'] = agg_df
 
-# select columns of interest
-clinical_df_filtered = clinical_df[['project_id', 'case_id','primary_diagnosis', 'ethnicity', 'gender', 'race', 'vital_status',
-'stage', 'year_of_diagnosis', 'age_at_diagnosis', 'year_of_birth', 'year_of_death', 'site_of_resection_or_biopsy', 
-'tissue_or_organ_of_origin', 'tumor_grade', 'days_to_death']]
+  # select columns of interest
+  clinical_df_filtered = clinical_df[['project_id', 'case_id','primary_diagnosis', 'ethnicity', 'gender', 'race', 'vital_status',
+  'stage', 'year_of_diagnosis', 'age_at_diagnosis', 'year_of_birth', 'year_of_death', 'site_of_resection_or_biopsy', 
+  'tissue_or_organ_of_origin', 'tumor_grade', 'days_to_death']]
 
-# ================ clean family history data ================
-family_df = family_df.replace("'--","")
-family_df_filtered = family_df[['case_id', 'project_id', 
-'relationship_gender', 'relationship_primary_diagnosis', 
-'relationship_type', 'relative_with_cancer_history']]
+  # ================ clean family history data ================
+  family_df = family_df.replace("'--","")
+  family_df_filtered = family_df[['case_id', 'project_id', 
+  'relationship_gender', 'relationship_primary_diagnosis', 
+  'relationship_type', 'relative_with_cancer_history']]
 
-# ================ merge both datasets ================
-full_df = pd.merge(clinical_df_filtered, family_df_filtered, on = "case_id", how = "left")
-full_df = full_df.replace("",np.nan)
-full_df
+  # ================ merge both datasets ================
+  full_df = pd.merge(clinical_df_filtered, family_df_filtered, on = "case_id", how = "left")
+  full_df = full_df.replace("",np.nan)
+  return full_df
 
+full_df = load_data()
 
-# Visualization 1
+# ============================================= Visualization 1 =============================================
 ## Part 1 and 2
+alt.data_transformers.enable('default', max_rows=None)
+st.write("# Overview of Cancer Trends")
+
 # select columns of interest and drop NAs
 Q1 = full_df[['tissue_or_organ_of_origin', 'gender', 'ethnicity', 
               'relative_with_cancer_history', 'relationship_type', 
@@ -50,14 +57,14 @@ Q1["relative_with_cancer_history"] = np.where(Q1["relative_with_cancer_history"]
 Q1 = Q1.dropna() 
 
 # === bar chart 1: number of cases per cancer type with relatives who also have a history of cancer ===
- 
+st.write("### Explore Trends in Family History")
 # add selector 
 select_cancer = alt.selection_single(encodings=["x"])
 
-num_relatives_cancer_base = alt.Chart(Q1).properties(width = 400, height = 200)
+num_relatives_cancer_base = alt.Chart(Q1).properties(height = 300)
 num_relatives_cancer = num_relatives_cancer_base.mark_bar().encode(
   x = "tissue_or_organ_of_origin:N", 
-  y = alt.Y("relative_with_cancer_history", aggregate = "sum"), 
+  y = alt.Y("relative_with_cancer_history", aggregate = "sum", scale = alt.Scale(type = "log")), 
   color = "tissue_or_organ_of_origin:N",
   tooltip = ["sum(relative_with_cancer_history)","tissue_or_organ_of_origin"],
   opacity=alt.condition(select_cancer, alt.value(1), alt.value(0.2))
@@ -68,24 +75,37 @@ num_relatives_cancer = num_relatives_cancer_base.mark_bar().encode(
 # === bar chart 2 === 
 num_relatives_cancer2 = num_relatives_cancer_base.mark_bar().encode(
     x = alt.X("relationship_primary_diagnosis:N", sort = '-y'),
-    y = alt.Y(aggregate = "count"),
+    y = alt.Y(aggregate = "count", scale = alt.Scale(type = "log")),
     color = alt.Color("relationship_type"),
     tooltip = ["relationship_primary_diagnosis", "relationship_type","count(relationship_type)"]
 ).transform_filter(
     select_cancer
 )
 
-num_relatives_cancer & num_relatives_cancer2
+both_charts = num_relatives_cancer | num_relatives_cancer2
+both_charts = both_charts.resolve_scale(color='independent')
+st.altair_chart(both_charts, use_container_width=True)
 
-# Visualization 2
-# Part1
-alt.data_transformers.enable('default', max_rows=None)
+# ============================================= Visualization 2 =============================================
+st.write("### Explore Trends in the Number of Diagnosis Across Years")
+
+# add option for users to select a few cancers 
+default_cancers = [
+    "Bone marrow",
+    "Breast, NOS",
+    "Cervix uteri",
+    "Kidney, NOS",
+    "Endometrium",
+    "Upper lobe, lung",
+    "Prostate gland",
+    "Testis, NOS"
+]
+cancers = st.multiselect(label = "Cancers", options = full_df['tissue_or_organ_of_origin'].unique(), default = default_cancers)
+subset = full_df[full_df["tissue_or_organ_of_origin"].isin(cancers)]
 
 # select columns of interest and drop NAs
-Q2 = full_df[['year_of_diagnosis','primary_diagnosis','case_id', 'tissue_or_organ_of_origin','gender']]
+Q2 = subset[['year_of_diagnosis','primary_diagnosis','case_id', 'tissue_or_organ_of_origin','gender']]
 Q2 = Q2.dropna()
-
-# =================== visualization for task 2 ===================
 
 # line chart
 num_diagnosis_cancer_year = alt.Chart(Q2).mark_line(point=alt.OverlayMarkDef()).encode(
@@ -93,8 +113,8 @@ num_diagnosis_cancer_year = alt.Chart(Q2).mark_line(point=alt.OverlayMarkDef()).
   y = alt.Y(aggregate = "count"), 
   color = "tissue_or_organ_of_origin:N",
   tooltip = [alt.Tooltip("tissue_or_organ_of_origin",title='Tissue or organ of origin'),alt.Tooltip("year_of_diagnosis:O",title='Year of diagnosis'), alt.Tooltip("count(year_of_diagnosis)",title='Count')]
-)
-num_diagnosis_cancer_year
+).properties(width = 800, height = 400)
+st.altair_chart(num_diagnosis_cancer_year, use_container_width=True)
 
 # Part2
 # bar chart 
@@ -103,14 +123,13 @@ num_diagnosis_cancer = alt.Chart(Q2).mark_bar().encode(
   y = alt.Y(aggregate = "count"),
   color = 'gender', 
   tooltip = ["tissue_or_organ_of_origin","gender", "count(tissue_or_organ_of_origin)"]
-)
-num_diagnosis_cancer
+).properties(width = 800, height = 400)
+st.altair_chart(num_diagnosis_cancer, use_container_width=True)
 
-#Visualization 3
+# ============================================= Visualization 3 =============================================
+st.write("### Explore Trends in Age of Cancer Diagnosis Throughout the Years")
 ## Part 1 & part 2
-pd.to_numeric(Q3['age_at_diagnosis'])/365
-Q3['age_at_diagnosis_year'] = pd.to_numeric(Q3['age_at_diagnosis'])/365
-Q3 = full_df[['year_of_diagnosis','age_at_diagnosis','tissue_or_organ_of_origin','gender']]
+Q3 = subset[['year_of_diagnosis','age_at_diagnosis','tissue_or_organ_of_origin','gender']]
 Q3['age_at_diagnosis_year'] = pd.to_numeric(Q3['age_at_diagnosis'])/365
 Q3 = Q3.dropna()
 
@@ -137,10 +156,11 @@ num_diagnosis_age_cancer2 = base.mark_boxplot().encode(
 )
 num_diagnosis_age_cancer & num_diagnosis_age_cancer2
 
-# Visualization 4
+# ============================================= Visualization 4 =============================================
+st.write("### Explore Associations in Age of Diagnosis with Age of Death")
 ## Part 1
 # scatter plot between average age of death and diagnosis for each cancer type
-Q4 = full_df[['year_of_diagnosis','age_at_diagnosis','tissue_or_organ_of_origin','year_of_death', 'year_of_birth']]
+Q4 = subset[['year_of_diagnosis','age_at_diagnosis','tissue_or_organ_of_origin','year_of_death', 'year_of_birth']]
 Q4['age_of_death'] = pd.to_numeric(Q4['year_of_death']) - pd.to_numeric(Q4['year_of_birth'])
 Q4['age_at_diagnosis_year'] = pd.to_numeric(Q4['age_at_diagnosis'])/365
 Q4 = Q4.dropna()
@@ -153,7 +173,7 @@ diagnosis_death_age = alt.Chart(Q4).mark_circle(size=60, opacity = 0.7).encode(
   tooltip = ["mean(age_of_death)","mean(age_at_diagnosis_year)","tissue_or_organ_of_origin"]
 )
 
-diagnosis_death_age
+st.altair_chart(diagnosis_death_age, use_container_width=True)
 
 ## Part3
 diagnosis_age_distribution = alt.Chart(Q4).mark_boxplot(opacity = 0.8).encode(
@@ -167,9 +187,11 @@ death_age_distribution = alt.Chart(Q4).mark_boxplot(opacity = 0.8).encode(
     color = "tissue_or_organ_of_origin:N"
 )
 
-diagnosis_age_distribution & death_age_distribution
+v4_both = diagnosis_age_distribution | death_age_distribution
+st.altair_chart(v4_both, use_container_width=True)
 
-# Visualization 6
+
+# ============================================= Visualization 6 =============================================
 #subset one cancer type
 Q6 = full_df[full_df["tissue_or_organ_of_origin"] == "Lung, NOS"]
 Q6 = full_df[['stage', 'ethnicity', 'gender', 'age_at_diagnosis']]
